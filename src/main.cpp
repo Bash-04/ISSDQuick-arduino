7781bfa8cf6c63496448c180d0b798eac6fe3b33
Bas Bakens <bas.bakens@live.nl>
2022-11-10 09:57:16 +0100

    Servo toegevoegd
    Servo library (het slot) toegevoegd. functies open en sluit slot toegevoegd waarbij de Servo in de goede hoek gaat staan.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,28 @@
 #include <Arduino.h>
+#include <Servo.h>
+
+Servo servo;
+int gesloten = 140;
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Hello World");
+  servo.attach(3);
+  servo.write(gesloten);
 }
 
-
+void SluitKluis(){
+  servo.write(140);
+  delay(800);
+}
+void OpenKluis(){
+  servo.write(42);
+  delay(800);
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+
 } 
